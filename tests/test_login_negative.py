223c677b13e8cb211cc4418346c4e270ceedbf37
mzvar <mirekzvar@gmail.com>
2018-09-12 17:01:21 +0300

    Add config

--- tests/test_login_negative.py
+++ tests/test_login_negative.py
@@ -1,5 +1,9 @@
 from desktop.windows.login_window import LoginWindow, AlertWindow
 import pytest
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
 
 
 @pytest.mark.usefixtures("setup_tear_down")
@@ -7,7 +11,7 @@ class TestClass:
 
     def test_enter_logo(self):
         login_form = LoginWindow(self.driver)
-        login_form.enter_pasword('=Nws@A0')
+        login_form.enter_pasword(config.get('credential', 'wrong_password'))
         login_form.click_ok()
         alert_window = AlertWindow(self.driver)
         assert alert_window.verify_shown_alert()
