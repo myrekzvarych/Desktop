ff732517c0b91f501fe4d1bd014b55c610f80c21
mzvar <mirekzvar@gmail.com>
2018-09-12 17:00:12 +0300

    Add config.ini

--- tests/test_login_positive.py
+++ tests/test_login_positive.py
@@ -1,6 +1,10 @@
 from desktop.windows.login_window import LoginWindow
 from desktop.windows.main_window import MainWindow
 import pytest
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
 
 
 @pytest.mark.usefixtures("setup_tear_down")
@@ -8,7 +12,7 @@ class TestClass:
 
     def test_enter_logo(self):
         login_form = LoginWindow(self.driver)
-        login_form.enter_pasword('=Nws@A0Y')
+        login_form.enter_pasword(config["credential"]["password"])
         login_form.click_ok()
         main_window = MainWindow(self.driver)
         assert main_window.check_main_frame() == 'title bar'
